6a08e4e9d4554c210dc63dccab171831d0aeb8ab
rajeshsam6698 <rajeshsam6698@gmail.com>
2022-02-21 18:12:49 +0530

    string manipulation

---
+++ String Manipulation/StringExercise1.java
@@ -0,0 +1,56 @@
+class StringExercise1
+{
+	public static String concatAdd(String str1,String str2)
+	{
+		String s=str1+" "+str2;
+		String ans="";
+		String[] str=s.split(" ");
+		for(int i=0;i<str.length;i++)
+		{
+			ans=ans+str[i];	
+		}
+		return ans;
+	}
+	public static String reverse(String str)
+	{
+		int len=str.length();
+		String result="";
+		char[] ch=new char[len];
+		for(int i=len-1;i>=0;i--)
+		{
+			 result=result+String.valueOf(str.charAt(i));	
+		}
+		return result;
+	}
+	protected static int paddlingIndex(String str1,String str2)
+	{
+		for(int i=0;i<str1.length();i++)
+		{
+			if(str2.charAt(0)==str1.charAt(i))
+			return i;
+		}	
+		return 0;		
+	}
+	public static boolean isDigit(String str)
+	{
+		int len=str.length();
+		 
+		for(int i=0;i<len;i++)
+		{
+			char ch=str.charAt(i);
+			if(ch>='0' && ch<='9')
+				continue;
+			else
+				return false;	
+		}
+		return true; 	
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Remove Spaces and Concat String: "+concatAdd("Rajesh"," Sam"));
+		System.out.println("Reverse String: "+reverse("Rajesh sam"));
+		System.out.println("Paddling Index: "+paddlingIndex("Sam Rajesh","Rajesh"));
+		System.out.println("Digit or Not: " +isDigit("5"));
+	}
+}
